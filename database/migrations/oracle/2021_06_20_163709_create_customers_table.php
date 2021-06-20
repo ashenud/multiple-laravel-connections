b56df4e3c5ff57da5f407b807e7b7e880c4d2656
Ashen Udithamal <udithamal.lk@gmail.com>
2021-06-20 23:06:03 +0530

    oracle database connected

---
+++ database/migrations/oracle/2021_06_20_163709_create_customers_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('oracle')->create('customers', function (Blueprint $table) {
+            $table->bigIncrements('cus_id');
+            $table->string('cus_name')->nullable();
+            $table->integer('cus_no')->nullable();
+            $table->string('cus_add1')->nullable();
+            $table->string('cus_add2')->nullable();
+            $table->string('cus_add3')->nullable();
+            $table->string('cus_tel')->nullable();
+            $table->string('cus_fax')->nullable();
+            $table->string('cus_email')->nullable();
+            $table->dateTime('cus_adddate')->nullable();
+            $table->string('cus_addtime')->nullable();
+
+            $table->index('cus_no');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::connection('oracle')->dropIfExists('customers');
+    }
+}
