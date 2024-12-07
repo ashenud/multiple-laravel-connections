63e9748590290c2cdc637df75ebdeebf64027468
Ashen Udithamal <udithamal.lk@gmail.com>
2024-12-07 22:03:32 +0530

    Add comments on pending oracle configurations

--- docker/oracle/init.sh
+++ docker/oracle/init.sh
@@ -1,11 +1,16 @@
+# TODO: This file is currently unused. Update it to create users based on `.env` configurations.
+# Reference: https://github.com/ashenud/multiple-laravel-connections/issues/3
+
 #!/bin/bash
 
 # Start the Oracle Listener
 echo "Starting Oracle Listener..."
+
 lsnrctl start
 
 # Start the Oracle Database
 echo "Starting Oracle Database..."
+
 sqlplus / as sysdba <<EOF
 STARTUP;
 ALTER PLUGGABLE DATABASE ALL OPEN;
