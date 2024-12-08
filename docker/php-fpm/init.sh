f438982dac83301422b00cd564bda85e4062ef12
Ashen Udithamal <udithamal.lk@gmail.com>
2024-12-08 18:04:03 +0530

    Update comments

--- docker/php-fpm/init.sh
+++ docker/php-fpm/init.sh
@@ -6,6 +6,10 @@ chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 # Set permissions for storage directory
 chmod -R 777 /var/www/html/storage
 
+# Allows Git to safely operate within the specified directory, `/var/www/html`.
+# This is useful for avoiding 'fatal: unsafe directory' errors when working with Git in Docker or other non-standard environments.
+git config --global --add safe.directory /var/www/html
+
 # Run composer update
 composer update
 
